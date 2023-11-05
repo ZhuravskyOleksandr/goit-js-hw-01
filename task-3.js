b5de784296632be90766041afc8a69d5df2cfc54
ZhuravskyOleksandr <zhuravsky31@gmail.com>
2023-11-06 00:40:58 +0200

    first-try

---
+++ task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingValue = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalElementWidth = contentWidth + 2 * (paddingValue  + borderWidth) ;
+    return totalElementWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
